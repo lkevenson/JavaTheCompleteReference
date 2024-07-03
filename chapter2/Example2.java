7a80fdcf06e4d0c6ce61681ccb7784db67a4f2c2
Kevenson Leger <leger.kevenson@gmail.com>
2024-07-02 21:04:13 -0400

    Initial commit

---
+++ chapter2/Example2.java
@@ -0,0 +1,21 @@
+
+/*
+ Here is another short example.
+ Call the file "Example2.java"
+ */
+class Example2 {
+  
+  public static void main(String[] args) {
+    
+    int num; // this declares a variable call num
+
+    num = 100; // this assigns num the value 100
+
+    System.out.println("This is num: " + num );
+
+    num = num * 2;
+
+    System.out.print("The value of num * 2 is ");
+    System.out.println(num);
+  }
+}
---
+++ chapter2/IfExample.java
@@ -0,0 +1,24 @@
+
+/*
+ Demonstrate the if
+
+ Call the file "IfExample.java"
+ */
+class IfExample {
+  public static void main(String[] args) {
+    int x,y;
+    x = 10;
+    y = 20;
+
+    if(x < y)System.out.println(" x is less than y");
+
+    x = x * 2;
+    if( x == y) System.out.println("x now equal to y");
+
+    x = x * 2;
+    if( x > y) System.out.println("x now is greater than y");
+
+    // This won't display anything
+    if(x == y) System.out.println("Ypu won't see this");
+  }
+}
