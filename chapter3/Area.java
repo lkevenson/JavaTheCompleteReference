e99d57964bbfffd39e88cf3aef0593a908d69d2e
Kevenson Leger <leger.kevenson@gmail.com>
2024-07-05 15:08:27 -0400

    Area Compute the area of a circle

---
+++ chapter3/Area.java
@@ -0,0 +1,12 @@
+// Compute the Area of a circle
+class Area{
+  public static void main(String[] args) {
+    double pi,r,a;
+    r = 10.8; // radius of circle
+    pi = 3.1416; // pi approximately
+    a = pi * r * r; // compute area
+
+
+    System.out.println("Area of the Circle is " + a);
+  }
+}
