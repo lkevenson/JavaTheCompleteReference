231854a68302e7ab5c6c661145098b4f6e9fd64b
Kevenson Leger <leger.kevenson@gmail.com>
2024-07-08 18:36:39 -0400

    DynInit Demonstrate dynamic initialization

---
+++ chapter3/DynInit.java
@@ -0,0 +1,14 @@
+// Demonstrate dynamic initialization
+public class DynInit {
+
+  public static void main(String[] args) {
+    double a = 3.0, b = 4.0;
+
+    // c is dynamically initialized
+    double c = Math.sqrt(a * a + b * b);
+
+    System.out.println("Hypotemuse is " + c);
+  }
+
+
+}
