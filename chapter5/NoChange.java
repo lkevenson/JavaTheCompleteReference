048ea3e7853a98b076c58cf364449faf2b2b5450
Kevenson Leger <leger.kevenson@gmail.com>
2024-07-18 08:00:28 -0400

    NoChange.java The for-each loop is essentially read-only.

--- chapter5/NoChange.java
+++ chapter5/NoChange.java
@@ -4,14 +4,14 @@ public class NoChange {
         int[] nums = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
         for (int x : nums) {
-            System.out.println(x + " ");
+            System.out.print(x + " ");
             x = x * 10; // no effect on nums
         }
 
         System.out.println();
 
         for (int x : nums)
-            System.out.println(x + " ");
+            System.out.print(x + " ");
 
         System.out.println();
     }
