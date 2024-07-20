581b02a01f92ce67137baa71fd8827a2d9c0a0a8
Kevenson Leger <leger.kevenson@gmail.com>
2024-07-20 15:52:49 -0400

    BoxDemo3 This program includes a method inside the class.

--- chapter6/Box.java
+++ chapter6/Box.java
@@ -2,4 +2,11 @@ class Box {
     double width;
     double height;
     double depth;
+
+
+    // display volume of box
+    void volume(){
+        System.out.print("Volume is ");
+        System.out.println(width * height * depth);
+    }
 }
